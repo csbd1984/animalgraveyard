fb2db7dcdd1330377ee48939acb3149df332e170
csbd1984 <117699835+csbd1984@users.noreply.github.com>
2023-05-20 15:13:24 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 // Define the animal slaughter rates in graves per second
-const kettleSlaughterRate = 100000 / 2; // Estimated kettle slaughter rate per year
-const chickenSlaughterRate = 1000000 / 2; // Estimated chicken slaughter rate per year
-const pigSlaughterRate = 500000 / 2; // Estimated pig slaughter rate per year
-
+const cattleSlaughterRate = 1 / 5; // 1 grave every 5 seconds
+const pigGraveRate = 47 / 1; // 1 grave every 0.021 seconds
+const chickenGraveRate = 2200 / 1; // 1 grave every 0.00045 seconds
 
 // Function to animate the graveyard expansion
 function animateGraveyard(graveyardId, slaughterRate) {
-    const graveyard = document.getElementById(graveyardId);
-    let graveCount = 0;
+  const graveyard = document.getElementById(graveyardId);
+  let graveCount = 0;
 
-    // Update the graveyard every second
-    setInterval(() => {
-        graveCount += slaughterRate;
-        graveyard.style.backgroundSize = `${graveCount}px ${graveCount}px`;
-    }, 1000);
+  // Update the graveyard every second
+  setInterval(() => {
+    graveCount += slaughterRate;
+    graveyard.style.backgroundSize = `${graveCount}px ${graveCount}px`;
+  }, 1000);
 }
 
-// Animate the kettle graveyard
-animateGraveyard('kettle-graveyard', kettleSlaughterRate);
+// Add an event listener to each menu item
+const menuItems = document.querySelectorAll('.menu a');
+menuItems.forEach(item => {
+  item.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent the default link behavior
+    const animalPage = item.getAttribute('href'); // Get the animal page URL
+    window.location.href = animalPage; // Navigate to the animal page
+  });
+});
 
-// Animate the chicken graveyard
-animateGraveyard('chicken-graveyard', chickenSlaughterRate);
+// Check the current page URL to determine which animal page is being displayed
+const currentPageURL = window.location.pathname;
+
+// Animate the kettle graveyard if it's the current page
+if (currentPageURL.includes('kettle.html')) {
+  animateGraveyard('kettle-graveyard', kettleSlaughterRate);
+}
+
+// Animate the chicken graveyard if it's the current page
+if (currentPageURL.includes('chicken.html')) {
+  animateGraveyard('chicken-graveyard', chickenSlaughterRate);
+}
+
+// Animate the pig graveyard if it's the current page
+if (currentPageURL.includes('pig.html')) {
+  animateGraveyard('pig-graveyard', pigSlaughterRate);
+}
+
+// Animate the cattle graveyard
+animateGraveyard('cattle-graveyard', cattleSlaughterRate);
 
 // Animate the pig graveyard
-animateGraveyard('pig-graveyard', pigSlaughterRate);
+animateGraveyard('pig-graveyard', pigGraveRate);
+
+// Animate the chicken graveyard
+animateGraveyard('chicken-graveyard', chickenGraveRate);
